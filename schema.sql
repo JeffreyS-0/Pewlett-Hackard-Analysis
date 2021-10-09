7d4e72c5c5219641aa1388dd9e1e136d495156bc
Jeffrey Sykora <69607218+JeffreyS-0@users.noreply.github.com>
2021-10-08 21:19:51 -0500

    Slight update to the schema

--- schema.sql
+++ schema.sql
@@ -47,7 +47,7 @@ CREATE TABLE dept_empl (
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	salary INT NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
